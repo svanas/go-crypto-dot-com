e1e1d76849a06a6babbca0f6759b226e35eb5997
svanas <svanas@runbox.com>
2020-03-29 12:20:46 +0200

    Bug: cannot unmarshal string into Go struct field Order.type of type int. Fixed.

--- order.go
+++ order.go
@@ -52,27 +52,27 @@ const (
 )
 
 type Order struct {
-	Id           int     `json:"id"`
-	Side         string  `json:"side"`
-	TotalPrice   float64 `json:"total_price,string"`
-	Fee          float64 `json:"fee,string"`
-	CreatedAt    int64   `json:"created_at"`
-	UpdatedAt    int64   `json:"updated_at"`
-	DealPrice    float64 `json:"deal_price,string"`
-	AvgPrice     float64 `json:"avg_price,string"`
-	CountCoin    string  `json:"countCoin"`
-	Source       int     `json:"source"`
-	Type         int     `json:"type"`
-	SideMsg      string  `json:"side_msg"`
-	Volume       float64 `json:"volume,string"`
-	Price        float64 `json:"price,string"`
-	SourceMsg    string  `json:"source_msg"`
-	StatusMsg    string  `json:"status_msg"`
-	DealVolume   float64 `json:"deal_volume,string"`
-	FeeCoin      string  `json:"fee_coin"`
-	RemainVolume float64 `json:"remain_volume,string"`
-	BaseCoin     string  `json:"baseCoin"`
-	Status       int     `json:"status"`
+	Id           int         `json:"id"`
+	Side         string      `json:"side"`
+	TotalPrice   float64     `json:"total_price,string"`
+	Fee          float64     `json:"fee,string"`
+	CreatedAt    int64       `json:"created_at"`
+	UpdatedAt    int64       `json:"updated_at"`
+	DealPrice    float64     `json:"deal_price,string"`
+	AvgPrice     float64     `json:"avg_price,string"`
+	CountCoin    string      `json:"countCoin"`
+	Source       int         `json:"source"`
+	Type         interface{} `json:"type"`
+	SideMsg      string      `json:"side_msg"`
+	Volume       float64     `json:"volume,string"`
+	Price        float64     `json:"price,string"`
+	SourceMsg    string      `json:"source_msg"`
+	StatusMsg    string      `json:"status_msg"`
+	DealVolume   float64     `json:"deal_volume,string"`
+	FeeCoin      string      `json:"fee_coin"`
+	RemainVolume float64     `json:"remain_volume,string"`
+	BaseCoin     string      `json:"baseCoin"`
+	Status       int         `json:"status"`
 }
 
 func (order *Order) GetSide() OrderSide {
@@ -86,9 +86,9 @@ func (order *Order) GetSide() OrderSide {
 
 func (order *Order) GetType() OrderType {
 	switch order.Type {
-	case 1:
+	case 1, "1":
 		return LIMIT
-	case 2:
+	case 2, "2":
 		return MARKET
 	default:
 		return ORDER_TYPE_UNKNOWN
